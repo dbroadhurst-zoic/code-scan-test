964bfc7aa52397c4fc08f95816ea948a33bcbfef
David Broadhurst <dbroadhurst@zoiclabs.com>
2022-01-20 17:18:59 -0500

    eval test

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ const tokenTest = '6542143b-d9eb-47fb-bcd5-4625e1fc78d2'
 const myNumber = 010
 
 function App() {
+  console.log(passwordTest)
+  console.log(tokenTest)
+  console.log(myNumber)
+
+  eval("console.log('eval')")
+
   return (
     <div className="App">
       <header className="App-header">
