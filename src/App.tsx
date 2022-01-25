a3f39bfe731f615716595751712e6c956d7c7999
David Broadhurst <dbroadhurst@zoiclabs.com>
2022-01-25 17:55:05 -0500

    dupe function test

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ const passwordTest = '!1234!'
 const tokenTest = '6542143b-d9eb-47fb-bcd5-4625e1fc78d2'
 const myNumber = 010
 
+function dupe() {}
+function dupe() {}
+
 function App() {
   console.log(passwordTest)
   console.log(tokenTest)
 
   eval("console.log('eval')")
-
-  bogusFunction()
 
   for (
     var i = 1;
